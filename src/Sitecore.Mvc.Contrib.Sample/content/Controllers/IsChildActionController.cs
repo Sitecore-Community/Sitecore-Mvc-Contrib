26c51e85e0a4ffec6e8f8dfd6238bd7942fc76a8
Kern Herskind Nightingale <kern@herskind.co.uk>
2013-08-16 19:34:33 +0100

    Added ChildAction Renderer sample page.

---
+++ src/Sitecore.Mvc.Contrib.Sample/content/Controllers/IsChildActionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class IsChildActionController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Establish if this controller is exhibiting ChildAction behavior 
+            ViewBag.IsChildAction = ControllerContext.IsChildAction;
+
+            // .. and let's see what Renderer is in use
+            var renderingContext = Sitecore.Mvc.Presentation.RenderingContext.CurrentOrNull;
+            if (renderingContext != null)
+                ViewBag.RenderingType = renderingContext.Rendering.RenderingType;
+
+            return View();
+        }
+    }
+}
